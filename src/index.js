bcd9324275d0de63c9e3d00df1b8833c28281c05
joje0327 <joje0327@naver.com>
2021-12-18 10:12:26 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import Youtube from './service/youtube';
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params:{key: process.env.REACT_APP_YOUTUBE_APIKEY},
+});
+const youtube = new Youtube(httpClient);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App youtube={youtube}  />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
